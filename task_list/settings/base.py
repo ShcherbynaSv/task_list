3281414ca7fb3fd7c4fb9162c7ea0aa0a7216132
Svitlana Shcherbyna <sw.shcherbina@gmail.com>
2026-09-11 22:11:00 +0300

    Move SECRET_KEY and DEBUG to environment variables

--- task_list/settings/base.py
+++ task_list/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f=tb4%yktficxfz5j$ct$q!s*9zhie6l9t4_b5vg((g-e^apew"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # Application definition
@@ -70,17 +65,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "task_list.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
---
+++ task_list/settings/prod.py
@@ -0,0 +1,7 @@
+from task_list.settings.base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
